a92b6ef8430b46e36e0d19f70331d846e728ad26
Abhinav Verma <abhinavverma658@gmail.com>
2024-02-21 01:12:39 +0530

    new commit

---
+++ printEvenNumber.js
@@ -0,0 +1,22 @@
+//1st approach 
+for(let i=0;i<=100;i++){
+    if(i%2==0){
+        console.log("Even Number by 1st approach:",i);
+    }
+}
+//2nd approach
+let i=0;
+while(i<=100){
+    if(i%2==0){
+        console.log("Even Number by 2nd approach: ",i);
+    }
+    i++;
+}
+//3rd Approach
+let j=0;
+do{
+    if(j%2==0){
+        console.log("Even Number 3rd approach: ",j);
+    }
+    j++
+}while(j<=100);
