37e69523f78e27bb18d5d39125d08df3cd093cbc
Abhinav Verma <abhinavverma658@gmail.com>
2024-02-22 23:35:02 +0530

    new commit

--- randomNumberGeneratorGame.js
+++ randomNumberGeneratorGame.js
@@ -1,1 +1,6 @@
-let num;
+let num=25;
+let num1=window.prompt("Guess the number: ");
+while(num!=num1){
+    num1=window.prompt("Wrong guess. Guess again!!")
+}
+console.log("Coongratulations! You guess right number.")
